39328c9ceb40bb2e674009a1be77d4c16d036dac
Liz <lizaharonian@gmail.com>
2018-11-27 09:27:13 +0200

    1

--- Main.py
+++ Main.py
@@ -1,1 +1,25 @@
-print ("Hi")
+import pandas as pd
+import numpy as np
+pd.set_option('max_columns', 120)
+pd.set_option('max_colwidth', 5000)
+
+# skip row 1 so pandas can parse the data properly.
+df = pd.read_csv('data.csv', low_memory=False)
+print(len(df.columns))
+print(df.columns)
+
+half_count = len(df) / 2
+df = df.dropna(thresh=half_count, axis=1) # Drop any column with more than 50% missing values
+print(len(df.columns))
+print(df.columns)
+
+pass
+
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# %matplotlib inline
+# plt.rcParams['figure.figsize'] = (12,8)
+
+
+#with open("data.csv") as data:
+
