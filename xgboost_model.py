4603e873f8c765a7c753c072cb6ae2e260137da5
Liz <lizaharonian@gmail.com>
2018-12-31 12:26:09 +0200

    XGboost changes

--- xgboost_model.py
+++ xgboost_model.py
@@ -25,20 +25,37 @@ def main(train_file, train_target_file, test_file, test_target_file):
                                  subsample=0.6,
                                  seed=42)
     model.fit(train_df, train_target_df)
+    y_list, y_hat_list = run_test(test_df, test_target_df, model)
+    average_evaluate(y_list, y_hat_list)
+    relative_error_evaluate(y_list,y_hat_list)
 
+def run_test(test_df, test_target_df, model):
     y_list = []
     y_hat_list = []
-
     for example, target in zip(test_df, test_target_df):
         pred = model.predict([example])
         y_list.append(target)
         y_hat_list.append(pred)
         print("Real value: {}, Predicted value: {}".format(target, pred))
-    print str(mean_absolute_error(y_list,y_hat_list))
+    return y_list,y_hat_list
 
-    pass
-def test_with_accuracy(test_df, test_target_df):
-    pass
+def average_evaluate(y_list,y_hat_list):
+    print str(mean_absolute_error(y_list, y_hat_list))
+
+def relative_error_evaluate(y_list,y_hat_list):
+    sum = 0
+    for y, y_hat in zip(y_list,y_hat_list):
+        if (y > y_hat):
+            relative_error = (1 - float(y_hat)/float(y)) * 100
+        else:
+            relative_error = (1 - float(y)/float(y_hat)) * 100
+        sum += relative_error
+        print str(relative_error)
+    print "total relative error: " + str(float(sum)/len(y_list))
+
+
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
